896edf5d46a5a7b483d2af8123934dab00792586
mdhell <mdhellstrom@gmail.com>
2018-06-28 00:03:37 +0000

    Update the datepicker functionality to prefill current selection

--- public/js/tools.js
+++ public/js/tools.js
@@ -1,11 +1,10 @@
+$( function() {
+  $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd', changeYear:true, constrainInput: false, gotoCurrent: false, yearRange: "1900:2200"});
+  $( "#format" ).on( "change", function() {
+    $( "#datepicker" ).datepicker( "option", "dateFormat", $( this ).val() );
+  });
+} );
 
-  $( function() {
-    $( "#datepicker" ).datepicker({ dateFormat: 'yy-mm-dd', changeYear:true, constrainInput: false, gotoCurrent: true, yearRange: "1900:2200"});
-    $( "#format" ).on( "change", function() {
-      $( "#datepicker" ).datepicker( "option", "dateFormat", $( this ).val() );
-    });
-  } );
-  
-  $( ".selector" ).datepicker({
-  gotoCurrent: true
-});
+// $( ".selector" ).datepicker({
+//   gotoCurrent: true
+// });
