47c7e9a5bdcc50513332aabd626d4c8df2b9f688
Michael Hellstrom <mdhellstrom@gmail.com>
2022-01-23 10:30:22 -0500

    console log added

--- app.js
+++ app.js
@@ -29,10 +29,10 @@ app.get("/results", function(req, res) {
     var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=" + startDate + "&end_date=" + startDate + "&api_key=5LtjDvkS64nPQYbfPudOl8szw8gwT9Z03i4ScysJ";
     // var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=2016-09-07&end_date=2016-09-08&api_key=98OtiAcpdjH97wT43K81GrWDll5iz3hmEpMVWAYX";
     request(url, function(err, response, body) {
-        app.use(timeout('5s'));
+
     if(!err && response.statusCode == 200) {
             var data = JSON.parse(body);
-            
+            console.log(data);
             // var data = JSON.parse(body);
             res.render("results", {data: data, startDate: startDate, endDate: endDate});
             
@@ -47,6 +47,7 @@ app.get("/results", function(req, res) {
             // console.log(req.query.page);
             // console.log(body);
             // res.render("results", {data: data});
+            
     } else {
         console.log("Sorry, there was some type of error...");
     }
@@ -54,26 +55,6 @@ app.get("/results", function(req, res) {
 // });
 });
 
-// begin timeout
-// var app = express()
-// app.post('/save', timeout('5s'), bodyParser.json(), haltOnTimedout, function (req, res, next) {
-//   savePost(req.body, function (err, id) {
-//     if (err) return next(err)
-//     if (req.timedout) return
-//     res.send('saved as id ' + id)
-//   })
-// })
- 
-// function haltOnTimedout (req, res, next) {
-//   if (!req.timedout) next()
-// }
- 
-// function savePost (post, cb) {
-//   setTimeout(function () {
-//     cb(null, ((Math.random() * 40000) >>> 0))
-//   }, (Math.random() * 7000) >>> 0)
-// }
-// end timeout
 
 
 
